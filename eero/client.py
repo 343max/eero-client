b92c802cfe12cc3a727117e1366d7b641aab0b47
Sam McKelvie <dev@mckelvie.org>
2022-12-16 12:23:38 -0800

    fix erroneous use of "is not" rather than !=

--- eero/client.py
+++ eero/client.py
@@ -9,7 +9,7 @@ class Client(object):
 
     def _parse_response(self, response):
         data = json.loads(response.text)
-        if data['meta']['code'] is not 200 and data['meta']['code'] is not 201:
+        if data['meta']['code'] != 200 and data['meta']['code'] != 201:
             raise ClientException(data['meta']['code'],
                                   data['meta'].get('error', ""))
         return data.get('data', "")
